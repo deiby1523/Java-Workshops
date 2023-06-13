3a429ebf4fc980b10cb717e0f68f6bf3cdd776bf
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-12 21:05:54 -0500

    Latest configurations in gitignore

--- Introduction to Java/Hello-World/src/Greeting.java
+++ Introduction to Java/Hello-World/src/Greeting.java
@@ -1,7 +1,13 @@
 public class Greeting {
+    // The name of the class must be the same as the name of the file, and it must start with a capital letter.
+
+    // Main Method
     public static void main(String[] args) {
 
+        // This is a sentence, they must end in ;
         System.out.println("Hello World!");
 
     }
+
 }
+
