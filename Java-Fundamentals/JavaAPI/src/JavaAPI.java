09626253e5a26ad92c1c06a98cbb733c4e681e8f
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-13 18:40:50 -0500

    java API and imports

---
+++ Java-Fundamentals/JavaAPI/src/JavaAPI.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaAPI {
+    public static void main(String[] args) {
+
+        Scanner entry;
+
+        Locale object1;
+
+        List list;
+
+    }
+}
