550222afe88f8d1f2e1c6fb890b356db15d6942e
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-13 00:30:32 -0500

    Variables and Primitive Data Types

---
+++ Introduction-to-Java/Tipos-de-Datos/src/Variables.java
@@ -0,0 +1,66 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+        // Primitive data types
+
+        // Integer
+        //      Int: 4 bytes - From -2.147.483.648 to 2.147.438.647
+        //      Short: 2 bytes - From -32.768 to 32.767
+        //      Long: 8 bytes - Too large, They are suffixed with L
+        //      Byte: 1 byte - From -128 to 127
+
+        // Decimals
+        //      Float: 4 bytes - Approximately 6 to 7 decimal places, They are suffixed with F
+        //      Double: 8 bytes - Approximately 15 decimal places
+
+        // Char
+        //      They are used to represent unicode characters, they are enclosed in single quotation marks '#'
+
+        // Boolean
+        //      They are data of logical type, used to represent true or false.
+
+
+
+
+
+        // Variable
+        //      Space in the computer's memory where a value will be stored that may change during program execution.
+
+        // Variable declaration
+        //      Data type + variable name
+
+        // Initializing a variable
+        //      Variable name = Value
+        //      Java does not allow the use of variables that have not been initialized before.
+
+
+        // Declaration
+        byte age;
+
+        // Initialization
+        age = 19;
+
+        // System.out.println(age);
+
+        int number = 20;
+
+        // Constant
+        //      Space in the computer's memory where a value will be stored that Cannot change during program execution
+
+        // Declaration
+        //      Final + Data Type + Variable name
+
+        // Initialization
+        //      Variable Name = Value
+
+
+        // Declaration
+        final double PI;
+
+        // Initialization
+        PI = 3.1416;
+        // It is recommended to declare and initialize in the same line and it is a good practice to write them in UPPERCASE.
+
+    }
+}
