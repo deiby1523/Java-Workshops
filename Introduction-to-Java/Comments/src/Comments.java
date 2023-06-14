8a648cd52a02fcb330d5f88b4900fe530ae93a04
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-13 22:20:11 -0500

    Comments in IntelliJ

---
+++ Introduction-to-Java/Comments/src/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // Comments in IntelliJ
+
+        // TODO
+        // NOTE
+        // FIXME
+        // WARNING
+
+    }
+}
