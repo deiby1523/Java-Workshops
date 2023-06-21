03f705b0bcd42bb595982643f1e7e6c511885f7a
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-20 23:26:31 -0500

    Solution of workshop 1 exercise 2

---
+++ Practices/Workshop_1/src/Exercise_2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise_2 {
+    /*
+
+    Create a code that allows the director of a company to calculate the new salary of an employee.
+    taking into account that the increase must be made according to the increase authorized by the
+    state for the current year(16%). Print the current salary, the value of the increase and the new salary.
+
+     */
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+        double salary;
+        double increment;
+        double newSalary;
+
+        System.out.println("Enter the employee salary");
+        salary = entry.nextDouble();
+
+        increment = salary * 0.16;
+        newSalary = salary + increment;
+
+        System.out.println( "The employee's current salary is $" + salary + "\n" +
+                            "The increment is $" + increment + "\n" +
+                            "The employee's new salary is $" + newSalary);
+
+    }
+}
