e669f19413faa6314c612ba40b398d3646a98a2a
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-06-25 20:32:18 -0500

    Solution of workshop 3 exercise 1

---
+++ Practices/Workshop_3/src/Exercise_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exercise_1 {
+    /*
+
+    Design a program that, given as input a one-dimensional array (vector) of
+    integers, obtains as output the sum of each of the elements of the vector or array.
+
+     */
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+        int n;
+        double sum = 0;
+
+
+        System.out.println("Enter the vector size:");   //  We ask the user for the length of the array
+        n = entry.nextInt();
+
+        double[] numbers = new double[n]; //  Create the array of N integers
+
+        for (int i = 0; i < n ; i++) {  //   We iterate N times to go through the array positions and add values
+            System.out.println("Enter the number " + (i+1) + ":");
+            numbers[i] = entry.nextDouble();
+        }
+
+        for (int i = 0; i < n ; i++) {  //   We use another FOR loop to sum up the array
+            sum += numbers[i];
+        }
+
+        System.out.println("The sum of the array numbers is: " + sum);
+
+
+
+    }
+}
