56546b6a3955640c4a52a4c617e47631e4dd52b9
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-07-17 18:14:48 -0500

    Array fundamentals

---
+++ Java-Fundamentals/Arrays/src/Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //  int[] myArray = new int[5];
+
+        //  myArray[0] = 5;
+        //  myArray[1] = 38;
+        //  myArray[2] = -15;
+        //  myArray[3] = 92;
+        //  myArray[4] =
+
+        int[] myArray = {1,2,3,4,5};
+
+
+        for (int number : myArray) {
+            System.out.println("number = " + number);
+        }
+
+        int length = myArray.length;
+        
+    }
+}
