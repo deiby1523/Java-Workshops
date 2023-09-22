5a4fc9c5fbe8f4261909089b06ee9bfd761fbf09
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-09-21 23:02:48 -0500

    Connection class

---
+++ JavaDatabase/JDBC_handling/src/data/Connection.java
@@ -0,0 +1,30 @@
+package data;
+
+import java.sql.*;
+
+public class Connection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "";
+
+    public static java.sql.Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+
+    public static void close(ResultSet rs) throws SQLException {
+        rs.close();
+    }
+
+    public static void close(Statement st) throws SQLException {
+        st.close();
+    }
+
+    public static void close(PreparedStatement st) throws SQLException {
+        st.close();
+    }
+
+    public static void close(Connection cn) throws CloneNotSupportedException {
+        cn.clone();
+    }
+
+}
