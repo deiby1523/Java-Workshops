0b62332c326c335e1122de86b8e4d5d79b37306d
deiby1523 <deibyfabianpradaquintero@gmail.com>
2023-09-21 23:05:22 -0500

    Person Class - Entity

---
+++ JavaDatabase/JDBC_handling/src/domain/Person.java
@@ -0,0 +1,96 @@
+package domain;
+
+public class Person {
+    private int personId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+    private String address;
+
+    public Person() {
+
+    }
+
+    public Person(int personId) {
+        this.personId = personId;
+    }
+
+    public Person(String firstName, String lastName, String email, int phoneNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Person(int personId, String firstName, String lastName, String email, int phoneNumber, String address) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person[" +
+                "personId=" + personId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", address='" + address + '\'' +
+                ']';
+    }
+
+}
